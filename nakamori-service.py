a15c0e5e3bed01b1f3092c471e1429795ad80ec8
bigretromike <bigretromike@monogatari.pl>
2017-02-10 13:01:19 +0100

    hello world.

--- nakamori-service.py
+++ nakamori-service.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import time
+import xbmc
+
+if __name__ == '__main__':
+    monitor = xbmc.Monitor()
+
+    while not monitor.abortRequested():
+        # Sleep/wait for abort for 10 seconds
+        if monitor.waitForAbort(10):
+            # Abort was requested while waiting. We should exit
+            break
+        xbmc.log("hello addon! %s" % time.time(), level=xbmc.LOGDEBUG)
