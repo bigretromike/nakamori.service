b71767a81613376ba8365d24e2e87853b5f643d9
da3dsoul <da3dsoul@gmail.com>
2019-02-09 07:11:28 -0500

    Start refactor. Start work on nakamori.service

--- nakamori-service.py
+++ nakamori-service.py
@@ -4,12 +4,33 @@
 import time
 import xbmc
 
+import nakamori_utils.nakamoritools as nt
+
+
+def handle_scrobbling():
+    """
+    Scrobbles the current position to Shoko and/or Trakt
+    :return None:
+    """
+    pass
+
+
 if __name__ == '__main__':
     monitor = xbmc.Monitor()
 
     while not monitor.abortRequested():
-        # Sleep/wait for abort for 10 seconds
-        if monitor.waitForAbort(10):
+        # Sleep/wait for abort for 2 seconds
+        if monitor.waitForAbort(2):
             # Abort was requested while waiting. We should exit
             break
-        xbmc.log("hello addon! %s" % time.time(), level=xbmc.LOGDEBUG)
+
+        # we are running, check the players for relevant playback
+        player = xbmc.Player()
+        if not player.isPlayingVideo():
+            continue
+
+        playing_file = player.getPlayingFile()
+        if playing_file is None or nt.server not in playing_file:
+            continue
+
+        handle_scrobbling()
